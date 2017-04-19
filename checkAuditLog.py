e9e20bd5f86d47f0870d8e0eb2e79bcfe2f2a6c9
BoyangDong <bdong.bobo@gmail.com>
2017-04-19 12:08:16 -0500

    Update server spreadsheet

--- checkAuditLog.py
+++ checkAuditLog.py
@@ -30,56 +30,46 @@ def get_server_objs():
 
 def fetch_log(obj):
 	newdrive = MountDrive("\\\\"+obj.ip+"\\C$", 'vadmin12', '131wang', 'victory')
-
 	s = obj.log_path 
 	toks = s.split("\\")
 	dir = "Z:"
 	for i in range(len(toks)-1):
 		dir = dir + "\\\\"+toks[i+1]
-
 	print "the audit logs path under mount drive is: ", dir
 	# get the log names with the given path of mount drive 
 	log_names = [f for f in os.listdir(dir) if isfile(join(dir, f))]
-	print "there are %i log files under this dir", len(log_names)
-
+	print "FILE NUMBER: %s" % len(log_names)
 	counter = 0
+	print "CHECKING AUDIT LOGS..."
 	for i in log_names:
-		with open(dir + "\\" + i) as f:
-			f.seek(0)
-			first_char = f.read(1)
-			if not first_char:
-				print "Empty file is found..."
-				write_log(obj, i)
-			else:
-				counter += 1
-
+		if os.stat(dir + "\\" + i).st_size == 0:
+			write_log(obj, i)
+		else:
+			continue
 	newdrive.unmount() 
 
 
 def write_log(obj, log_name):
-
-	print "Writing into database..."
 	db = MySQLdb.connect(host="localhost",
 	                     user="bdong",     
 	                     passwd="bdong",  
 	                     db="log_report")
-
 	cur = db.cursor()
-	
 	try:
 		cur.execute("""INSERT INTO loginfo VALUES (%s,%s,%s,%s)""",(obj.server_name, obj.ip, log_name, "EMPTY FILE"))
 		db.commit()
-
 	except:
 		db.rollback()
-
 	db.close()
-	print "Finish updating"...""
 
+
+def send_email():
+	pass
 
 if __name__ == '__main__':
-
+	# fetch the list of servers 
 	get_server_objs()
+	# check the logs under each server 
 	for i in range(len(servers)):
 		print "--- Working on the Server: " + servers[i].server_name + "---"
 		fetch_log(servers[i])
