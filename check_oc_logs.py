5f0724c1a57ad04162d3b55a3d1154090d417eae
BoyangDong <bdong.bobo@gmail.com>
2017-06-09 13:37:19 -0500

    Check in for operation on single zip folder with no remove function

--- check_oc_logs.py
+++ check_oc_logs.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-  
-
 import paramiko
+import time 
+import os 
+import zipfile
+from time import gmtime, strftime 
+from ftp import upload_files
 
-pkey = 'C:\\Users\\boyang.dong\\.ssh\\sftpbudo-id_rsa'
+now = time.time() #current time in milliseconds
+current_date = strftime('%Y%m%d', gmtime())
+
+
 
 # Open a transport
 host = "ftp.optionscity.com" #64.74.102.118
@@ -12,6 +19,7 @@ transport = paramiko.Transport((host, port))
 # Auth
 password = "0duBptFsNow@!"
 username = "sftpbudo"
+pkey = r'%s' % 'C:/Users/boyang.dong/.ssh/sftpbudo-id_rsa'
 key=paramiko.RSAKey.from_private_key_file(pkey,password=password)  
 
 transport.connect(username=username, pkey=key)
@@ -19,19 +27,54 @@ transport.connect(username=username, pkey=key)
 # Go!
 sftp = paramiko.SFTPClient.from_transport(transport)
 
-file_path_root = 'instance1/audit_logs/'
-local_path_root = 'e:/Repos/Project_1/test_logs/OptionsCity/'
+file_path_root = r'%s' % 'instance1/audit_logs/'
+local_path_root = r'%s' %'e:/Repos/Project_1/test_logs/OptionsCity/'
 
-file_path = ''.join([file_path_root, 'Budo-JerryAttlan_AuditTrail_20170529.zip'])#\\instance1\\audit_logs\\Budo-JerryAttlan_AuditTrail_20170529.zip'
-local_path = ''.join([local_path_root, 'newzip.zip'])
-#local_path = 'E:\\Repos\\Project_1\\test_logs\\OptionsCity\\Log1.zip'
-sftp.get(r'%s' % file_path, r'%s' % local_path)
+file_path = ''.join([file_path_root, 'Budo-JerryAttlan_AuditTrail_20170523.zip'])
+local_path = ''.join([local_path_root, 'Budo-JerryAttlan_AuditTrail_20170523.zip'])
+
+counter = 0
+#Iterate zip folders sequencially 
+# Create the zipped folder 
+zipped_folder_name = ''.join(['_'.join(["SERVER_TBD", current_date]), '.zip']) 
+myZipFile = zipfile.ZipFile(zipped_folder_name, "w")
+
+'''
+folders = sftp.listdir(file_path_root)
+for i in range(len(folders)):
+	print sftp.listdir(file_path_root)[i] #Budo-JerryAttlan_AuditTrail_20170523.zip
+	counter = counter + 1
+'''
+'''Unzip to local dir 
+sftp.get(file_path, local_path)
+zip_ref = zipfile.ZipFile(local_path, 'r')
+zip_ref.extractall(local_path_root)
+zip_ref.close()
+'''
+for r, d, f in os.walk(local_path_root):
+	for log in f:
+		full_path = os.path.join(r, log)
+		date_file = os.path.getmtime(full_path)
+		if log.endswith(".log") or log.endswith(".txt"): 
+			if now - date_file <= 86400:
+				if os.path.getsize(full_path) == long(0):
+					counter = counter + 1
+					print counter
+					# record this empty log files into db
+				else:
+					#zip and upload to edfman  
+					file_name = full_path.split('/')[-1] #file name that shows up in the zipped folder is from the last token of full path 
+					myZipFile.write(full_path, file_name, zipfile.ZIP_DEFLATED)
+					#with open(r'%s' %'e:/Repos/Project_1/') as f:
+					upload_files(zipped_folder_name)
+					# zip folders need to be remoted.. 
+					#os.remove(zipped_folder_name)
+
 
 # Close
+#zip_ref.close()
 sftp.close()
 transport.close()
 
 # Reference Page 
 # https://stackoverflow.com/questions/3635131/paramikos-sshclient-with-sftp
-
-#sftp://sftpbudo@64.74.102.118:4242/instance1/audit_logs/Budo-JerryAttlan_AuditTrail_20170529.zip
