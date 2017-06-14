95a7a533f853a0e62d51543c9ec0c05d518384cc
BoyangDong <bdong.bobo@gmail.com>
2017-06-14 11:04:50 -0500

    Rival server info added

---
+++ RIVAL_logs.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-  
+
+rival_servers = {
+	"BUDO-DC3-RIVAL-1"	: ['10.64.0.14'	,	'Rod Valeroso'],
+	"BUDO-DC3-DC3-2"	: ['10.64.0.140',	'Anthony Akselrad'],
+	"Budo-350-RIVAL-1"	: ['10.62.0.9',		'Nike Roupas']
+}
